44c49d1ba05e357fc8d39ba689dc9b39106df181
liujia <liujia@recano.com.cn>
2025-02-25 15:35:49 +0800

    返推PB源文件，记录导出类型不一样！

--- Assets/Script/proto/build.bat
+++ Assets/Script/proto/build.bat
@@ -1,2 +1,10 @@
+#MyExternalMessage.proto生成的源文件中，使用ReadInt32而不是DEMO工程中的ReadSInt32，所以只是反编译了PB源文件，但生成的代码是不正确的。
+
+@echo off
+chcp 65001 > NUL
+
 echo "compile proto to c#"
-cmd /c "protoc *.proto --csharp_out=../entity/"
+protoc *.proto --csharp_out=../entity/
+
+pause
+
